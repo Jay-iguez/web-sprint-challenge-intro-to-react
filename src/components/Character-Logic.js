ec54bb82fa3eac72dc5134da62f09a0bebdc1066
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-07-05 12:51:31 -0700

    Added additional elements to CharacterLogic.JS

--- src/components/Character-Logic.js
+++ src/components/Character-Logic.js
@@ -10,19 +10,42 @@ export default function CharacterLogic(props) {
 
     const ParentCharacterContainer = styled.div`
     margin: 3rem 0;
+    position: relative;
     p {
         margin: 2rem 10%;
         background-color: rgba(136, 168, 183, 0.3);
+        &:hover {
+            background-color: #eff2f8;
+            color: black;
+        }
+    display: flex;
+    justify-content: center;
     }
     `
+
+    const BackCharacterButton = styled.div`
+    padding: 0 2rem;
+    background-color: rgba(136, 168, 183, 0.3);
+    position: absolute;
+    bottom: -26.5%;
+    left: 50%;
+    transform: translate(-50%)
+    `
+
+
     
     return (
+        <>
         <ParentCharacterContainer>
             {
               character.map(character => {
-            return <p>{character.name}</p>   
+            return <p>{(character.name).toUpperCase()}</p>   
             })    
             }
+            <BackCharacterButton>
+            BACK
+            </BackCharacterButton>
         </ParentCharacterContainer>
+        </>
     )
 }
